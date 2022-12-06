7a7218b5300ab7efbbad3f213f71bbe31c3a4e61
Alpvax <alpvax@gmail.com>
2022-12-06 20:38:06 +0000

    Fix d6p2

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 // IT WILL BE OVERWRITTEN EVERY TIME THE SOURCE IS BUILT!
 // ============================================================================
 
+
 mod cli;
 pub use cli::RunPart;
 
@@ -14,6 +15,6 @@ pub fn run(year: u16, day: u8, part: crate::RunPart) {
     match year {
         2022 => y2022::run_day(day, part),
         _ => panic!("Invalid year: no solutions have been written for {}", year),
-    }
-}
+            }
+        }
 pub mod y2022;
