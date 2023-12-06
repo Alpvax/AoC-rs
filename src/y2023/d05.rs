5450118790ac721eb956adf3ca820f72c06b0603
Alpvax <development@alpvax.uk>
2023-12-06 00:16:53 +0000

    Continue working on 2023 day 5 part 2. Wrong answer

--- src/y2023/d05.rs
+++ src/y2023/d05.rs
@@ -3,21 +3,79 @@ use std::cmp;
 type Id = u64;
 
 fn part1(seeds: &Vec<Id>, maps: &[Vec<(Id, Id, Id)>; 7]) -> Id {
-    seeds.iter().map(|seed| maps.iter().fold(*seed, |id, map|
-        if let Some((dst_start, src_start, _)) = map.iter().find(|(_, src_start, len)| src_start <= &id && src_start + len > id) {
-            dst_start + id - src_start
-        } else {
-            id
-        })).reduce(|a, b| cmp::min(a, b)).unwrap()
+    seeds
+        .iter()
+        .map(|seed| {
+            maps.iter().fold(*seed, |id, map| {
+                if let Some((dst_start, src_start, _)) = map
+                    .iter()
+                    .find(|(_, src_start, len)| src_start <= &id && src_start + len > id)
+                {
+                    dst_start + id - src_start
+                } else {
+                    id
+                }
+            })
+        })
+        .reduce(|a, b| cmp::min(a, b))
+        .unwrap()
 }
 
 fn part2(seeds: Vec<Id>, maps: [Vec<(Id, Id, Id)>; 7]) -> Id {
-    seeds.chunks(2).flat_map(|pair| (pair[0]..pair[0] + pair[1]).into_iter().map(|seed| maps.iter().fold(seed, |id, map|
-    if let Some((dst_start, src_start, _)) = map.iter().find(|(_, src_start, len)| src_start <= &id && src_start + len > id) {
-        dst_start + id - src_start
-    } else {
-        id
-    }))).reduce(|a, b| cmp::min(a, b)).unwrap()
+    let mut ids: Vec<_> = seeds
+        .chunks(2)
+        .map(|pair| (pair[0], pair[0] + pair[1]))
+        .collect();
+    for map in maps {
+        ids = ids
+            .into_iter()
+            .flat_map(|(start, end)| {
+                let (remaining, processed) = map.iter().fold(
+                    (vec![(start, end)], Vec::new()),
+                    |(remaining, mut processed), &(dst_start, src_start, src_len)| {
+                        let mut rem = Vec::new();
+                        let mut any_proc = false;
+                        dbg!((dst_start, src_start, src_len), &remaining);
+                        for (start, end) in remaining.iter().cloned() {
+                            if src_start > end || start > src_start + src_len {
+                                continue;
+                            }
+                            any_proc = true;
+                            let src_end = src_start + src_len;
+                            let dst_end = dst_start + src_len;
+                            match dbg!((start < src_start, end > src_end)) {
+                                (true, true) => {
+                                    processed.push((dst_start, dst_end));
+                                    rem.push((start, src_start));
+                                    rem.push((src_end, end));
+                                }
+                                (true, false) => {
+                                    processed.push((dst_start, dst_start + src_start - start));
+                                    rem.push((start, src_start));
+                                }
+                                (false, true) => {
+                                    processed.push((dst_start + start - src_start, dst_end));
+                                    rem.push((src_end, end));
+                                }
+                                (false, false) => {
+                                    processed.push((
+                                        dst_start + start - src_start,
+                                        dst_start + end - src_start,
+                                    ));
+                                }
+                            }
+                        }
+                        dbg!((if any_proc { rem } else { remaining }, processed))
+                    },
+                );
+                dbg!(processed.into_iter().chain(remaining.into_iter()))
+            })
+            .collect();
+    }
+    ids.into_iter()
+        .reduce(|(a, _), (b, _)| (cmp::min(a, b), 0))
+        .unwrap()
+        .0
 }
 
 crate::aoc! {
